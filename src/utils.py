06f0452f0374ea21f32a9ffc208c3f7ec419c10d
mahalakshmi-sabanayagam <maha.81193@gmail.com>
2023-11-15 17:15:19 +0100

    all different kernel measures

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,8 @@ from jaxtyping import Float, Integer
 import numpy as np
 import torch
 from torch_sparse import coalesce
+from sklearn.kernel_ridge import KernelRidge 
+from scipy.stats import wasserstein_distance
 
 def accuracy(logits: Float[torch.Tensor, "m"], 
              labels: Integer[torch.Tensor, "n"], 
@@ -66,3 +68,147 @@ def grad_with_checkpoint(outputs: Union[torch.Tensor, Sequence[torch.Tensor]],
         grad_outputs.append(input.grad.clone())
         input.grad.zero_()
     return grad_outputs
+
+def kernel_mmd(k, center=False, norm=False):
+    """Computes MMD of kernel as per https://en.wikipedia.org/wiki/Kernel_embedding_of_distributions
+    where P and Q are the two blocks of the kernel
+    assumes the nodes are sorted according to the class
+
+    center = True does mean zero kernel
+    norm = True does normalize the kernel by its Frobenius norm
+
+    Returns:
+        Scalar value : MMD of kernel k
+    """
+    n = k.shape[0]
+    m = int(n/2)
+
+    if center:
+        k = k-np.mean(k)
+    if norm:   
+        k = k/np.linalg.norm(k) 
+
+    # Term 1
+    c1 = 1 / ( m * (m - 1))
+    Kxx = k[:int(n/2),:int(n/2)]
+    A = np.sum(Kxx - np.diag(np.diagonal(Kxx)))
+
+    # Term II
+    c2 = 1 / (m * (m - 1))
+    Kyy = k[int(n/2):,int(n/2):]
+    B = np.sum(Kyy - np.diag(np.diagonal(Kyy)))
+
+    # Term III
+    c3 = 1 / (m * m)
+    C = np.sum(k[:int(n/2),int(n/2):]) + np.sum(k[int(n/2):,:int(n/2)])
+
+    # estimate MMD
+    mmd_est = c1 * A + c2 * B - 2 * c3 * C
+    return mmd_est
+
+def kernel_w2(k, center=False, norm=False):
+    """Computes wasserstein distance between P and Q 
+    where P and Q are the two blocks of the kernel
+    assumes the nodes are sorted according to the class
+
+    center = True does mean zero kernel
+    norm = True does normalize the kernel by its Frobenius norm
+
+    Returns:
+        Scalar value : Wasserstein distance of kernel k
+    """
+    n = k.shape[0]
+    if center:
+        k = k-np.mean(k)
+    if norm:   
+        k = k/np.linalg.norm(k) 
+
+    u1 = k[:int(n/2),:int(n/2)].reshape(-1)
+    u2 = k[int(n/2):,int(n/2):].reshape(-1)
+    u = np.concatenate((u1,u2), axis=0)
+    
+    v1 = k[:int(n/2),int(n/2):].reshape(-1)
+    v2 = k[int(n/2):,:int(n/2)].reshape(-1)
+    v = np.concatenate((v1,v2), axis=0)
+    
+    return wasserstein_distance(u, v)
+
+def block_diff(k, center=False, norm=False):
+    """Computes block difference of the kernel using the average of P - Q 
+    where P and Q are the two blocks of the kernel
+    assumes the nodes are sorted according to the class
+
+    center = True does mean zero kernel
+    norm = True does normalize the kernel by its Frobenius norm
+
+    Returns:
+        Scalar value : block difference of kernel k
+    """
+    n = k.shape[0]
+    if center:
+        k = k-np.mean(k)
+    if norm:   
+        k = k/np.linalg.norm(k) 
+        
+    diff = np.mean(k[:int(n/2),:int(n/2)]-k[:int(n/2),int(n/2):] + 
+                                     k[int(n/2):,int(n/2):]-k[int(n/2):,:int(n/2)])
+    return diff
+
+def class_separability(k, center=False, norm=False):
+    """Computes the statistical class separability of the kernel 
+    using the (average of P - Q)/(average of P + Q)
+    where P and Q are the two blocks of the kernel
+    assumes the nodes are sorted according to the class
+
+    center = True does mean zero kernel
+    norm = True does normalize the kernel by its Frobenius norm
+
+    Returns:
+        Scalar value : class separability of kernel k
+    """
+    n = k.shape[0]
+    if center:
+        k = k-np.mean(k)
+    if norm:   
+        k = k/np.linalg.norm(k)
+        
+    diff = np.mean(k[:int(n/2),:int(n/2)]-k[:int(n/2),int(n/2):] + 
+                                     k[int(n/2):,int(n/2):]-k[int(n/2):,:int(n/2)])
+    sum = np.mean(k[:int(n/2),:int(n/2)]+k[:int(n/2),int(n/2):] + 
+                                     k[int(n/2):,int(n/2):]+k[int(n/2):,:int(n/2)])
+    return diff/sum
+
+def kernel_alignment(k1, k2):
+    """Computes the alignment between kernels k1 and k2 
+    tr(k1 k2^T)/\sqrt{tr(k1 k1^T) . tr(k2 k2^T)}
+
+    Returns:
+        Scalar value : kernel alignment between k1 and k2
+    """
+    k1 = k1.float()
+    k2 = k2.float()
+    return torch.trace(k1 @ k2.T)/torch.sqrt((torch.trace(k1 @ k1.T) * torch.trace(k2 @ k2.T)))
+
+
+def kernel_cosine_sim(k1, k2):
+    """Sanity check for kernel_alignment -- kernel_cosine_sim should effectively do the same
+    
+    Returns:
+        Scalar value : kernel cosine sim between k1 and k2
+    """
+    k1 = k1.float()
+    k2 = k2.float()
+    k11 = torch.flatten(k1).reshape(1,-1)
+    k22 = torch.flatten(k2).reshape(1,-1)
+    cos = torch.nn.CosineSimilarity()
+    return cos(k11, k22)
+
+def sym_norm_kernel(S):
+    """ Symmetric normalize any matrix S
+    D^{-0.5} S D^{-0.5} where D is similar to the degree matrix 
+
+    Returns:
+        Scalar value : symmetric normalized matrix of S
+    """
+    Deg_inv = torch.diag(torch.pow(S.sum(axis=1), - 0.5))
+    return Deg_inv @ S @ Deg_inv
