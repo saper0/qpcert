93c98e7f924da7deefffbc2117bc1655c993913e
mahalakshmi-sabanayagam <maha.81193@gmail.com>
2024-06-17 10:26:49 +0200

    Added label flipping certificate

--- exp_ntk_certify_label.py
+++ exp_ntk_certify_label.py
@@ -248,77 +248,19 @@ def run(data_params: Dict[str, Any],
                                y_test=y, X_test=X, A_test=A, return_ntk=True)
         y_pred_trn, _ = ntk(idx_labeled=idx_labeled, idx_test=idx_labeled,
                                y_test=y, X_test=X, A_test=A, return_ntk=True)
-        if certificate_params["attack_nodes"] == "test":
-            idx_adv = rng.choice(idx_test, 
-                                 size=certificate_params["n_adversarial"],
-                                 replace=False)
-        elif certificate_params["attack_nodes"] == "train":
-            idx_adv = rng.choice(idx_trn, 
-                                 size=certificate_params["n_adversarial"],
-                                 replace=False)
-        elif certificate_params["attack_nodes"] == "all":
-            idx_known = np.concatenate((idx_labeled, idx_test)) 
-            idx_adv = rng.choice(idx_known, 
-                                 size=certificate_params["n_adversarial"],
-                                 replace=False)
-        else:
-            assert False, "Choose set of nodes to be attacked!"
 
         delta = certificate_params["delta"]
-        if not bool(certificate_params["delta_absolute"]):
-            delta = round(delta * 2 * mu[0].item(), 4)
         logging.info(f"Delta: {delta}")
-        y_ub, ntk_ub = ntk.forward_upperbound(idx_labeled, idx_test, idx_adv,
-                                              y, X, A, delta,
-                                              certificate_params["perturbation_model"],
-                                              return_ntk=True,
-                                              method=certificate_params["method"])
-
-        y_ub_trn, _ = ntk.forward_upperbound(idx_labeled, idx_labeled, idx_adv,
-                                              y, X, A, delta,
-                                              certificate_params["perturbation_model"],
-                                              return_ntk=True,
-                                              method=certificate_params["method"])
-        y_lb, ntk_lb = ntk.forward_lowerbound(idx_labeled, idx_test, idx_adv,
-                                              y, X, A, delta,
-                                              certificate_params["perturbation_model"],
-                                              return_ntk=True,
-                                              method=certificate_params["method"])
-        y_lb_trn, ntk_lb = ntk.forward_lowerbound(idx_labeled, idx_labeled, idx_adv,
-                                              y, X, A, delta,
-                                              certificate_params["perturbation_model"],
-                                              return_ntk=True,
-                                              method=certificate_params["method"])
         acc = utils.accuracy(y_pred, y[idx_test])
         acc_trn = utils.accuracy(y_pred_trn, y[idx_labeled])
-        acc_ub = utils.accuracy(y_ub, y[idx_test])
-        acc_lb = utils.accuracy(y_lb, y[idx_test])
-        acc_ub_trn = utils.accuracy(y_ub_trn, y[idx_labeled])
-        acc_lb_trn = utils.accuracy(y_lb_trn, y[idx_labeled])
-    # Trivial (1-Layer) Evasion Certifcation:
-    mask_no_adv_in_n = (A[:, idx_adv] > 0).sum(dim=1) == 0
-    A2 = A.matmul(A)
-    mask_no_adv_in_n2 = (A2[:, idx_adv] > 0).sum(dim=1) == 0
-    mask_no_adv_in_receptive_field = torch.logical_and(mask_no_adv_in_n, mask_no_adv_in_n2)
-    acc_cert_trivial = mask_no_adv_in_receptive_field[idx_test].sum().cpu().item() / len(idx_test)
-    # NTK Evasion Certificate:
-    acc_cert_robust_evasion = utils.certify_robust(y_pred, y_ub, y_lb)
-    acc_cert_unrobust_evasion = utils.certify_unrobust(y_pred, y_ub, y_lb)
     logging.info(f"Test accuracy: {acc}")
     logging.info(f"Train accuracy: {acc_trn}")
-    logging.info(f"Accuracy_lb_test: {acc_lb}")
-    logging.info(f"Accuracy_ub_test: {acc_ub}")
-    logging.info(f"Accuracy_lb_trn: {acc_lb_trn}")
-    logging.info(f"Accuracy_ub_trn: {acc_ub_trn}")
-    logging.info(f"Certified accuracy (evasion): {acc_cert_robust_evasion}")
-    logging.info(f"Certified accuracy (evasion, trivial): {acc_cert_trivial}")
-    logging.info(f"Certified unrobustness (evasion): {acc_cert_unrobust_evasion}")
-
+    
     # Poisoning Certificate
     svm_alpha = ntk.svm
-    is_robust_l, obj_l, opt_status_l = utils.certify_robust_label(
-            idx_labeled, idx_test, ntk_test, ntk_lb, ntk_ub, y, y_pred,
-            svm_alpha, C=model_params["regularizer"], M=1e3, Mprime=1e3
+    is_robust_l, obj_l, opt_status_l, y_opt_l = utils.certify_robust_label(
+            idx_labeled, idx_test, ntk_test, y, y_pred,
+            svm_alpha, l_flip=delta, C=model_params["regularizer"], M=1e3, Mprime=1e3
     )
     acc_cert = sum(is_robust_l) / y_pred.shape[0]
     acc_cert_u = 0 #not implemented
@@ -334,36 +276,18 @@ def run(data_params: Dict[str, Any],
     cond_regularized = torch.linalg.cond(ntk_labeled)
     min_ypred = torch.min(y_pred).cpu().item()
     max_ypred = torch.max(y_pred).cpu().item()
-    min_ylb = torch.min(y_lb).cpu().item()
-    max_ylb = torch.max(y_lb).cpu().item()
-    min_yub = torch.min(y_ub).cpu().item()
-    max_yub = torch.max(y_ub).cpu().item()
     min_ntklabeled = torch.min(ntk_labeled).cpu().item()
     max_ntklabeled = torch.max(ntk_labeled).cpu().item()
     avg_ntkunlabeled = torch.mean(ntk_unlabeled).cpu().item()
     min_ntkunlabeled = torch.min(ntk_unlabeled).cpu().item()
     max_ntkunlabeled = torch.max(ntk_unlabeled).cpu().item()
-    avg_ntklb = torch.mean(ntk_lb).cpu().item()
-    min_ntklb = torch.min(ntk_lb).cpu().item()
-    max_ntklb = torch.max(ntk_lb).cpu().item()
-    avg_ntkub = torch.mean(ntk_ub).cpu().item()
-    min_ntkub = torch.min(ntk_ub).cpu().item()
-    max_ntkub = torch.max(ntk_ub).cpu().item()
 
     if torch.cuda.is_available() and other_params["device"] != "cpu":
         torch.cuda.empty_cache()
-
     return dict(
         # general statistics
         accuracy_test = acc,
         accuracy_trn = acc_trn,
-        accuracy_ub_test = acc_ub,
-        accuracy_lb_test = acc_lb,
-        accuracy_ub_trn = acc_ub_trn,
-        accuracy_lb_trn = acc_lb_trn,
-        accuracy_cert_evasion_trivial = acc_cert_trivial,
-        accuracy_cert_evasion_robust = acc_cert_robust_evasion,
-        accuracy_cert_evasion_unrobust = acc_cert_unrobust_evasion,
         accuracy_cert_pois_robust = acc_cert,
         accuracy_cert_pois_unrobust = acc_cert_u,
         delta_absolute = delta,
@@ -373,12 +297,12 @@ def run(data_params: Dict[str, Any],
         y_worst_obj = obj_l,
         y_is_robust = is_robust_l,
         y_opt_status = opt_status_l,
+        y_flip = y_opt_l, 
         # split statistics
         idx_train = idx_trn.tolist(),
         idx_val = idx_val.tolist(),
         idx_labeled = idx_labeled.tolist(),
         idx_test = idx_test.tolist(),
-        idx_adv = idx_adv.tolist(),
         # data statistics
         csbm_mu = mu[0].item(),
         csbm_p = p,
@@ -387,16 +311,6 @@ def run(data_params: Dict[str, Any],
         # other statistics ntk / pred
         min_ypred = min_ypred,
         max_ypred = max_ypred,
-        min_ylb = min_ylb,
-        max_ylb = max_ylb,
-        min_yub = min_yub,
-        max_yub = max_yub,
-        avg_ntklb = avg_ntklb,
-        min_ntklb = min_ntklb,
-        max_ntklb = max_ntklb,
-        avg_ntkub = avg_ntkub,
-        min_ntkub = min_ntkub,
-        max_ntkub = max_ntkub,
         min_ntklabeled = min_ntklabeled,
         max_ntklabeled = max_ntklabeled,
         avg_ntkunlabeled = avg_ntkunlabeled,
