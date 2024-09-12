Added correct calculation for collective cert. if stopped early. Added memory limit. Fxied initial solution calc. Added multiclass experiments.

exp_ntk_certify_collective_label.py
@@ -219,6 +219,8 @@ def run(data_params: Dict[str, Any],
                                           other_params, seed)
     
     X, A, y, mu, p, q = get_graph(data_params, sort=True)
+    n_edges = np.sum(A) / 2
+    logging.info(f"Sampled {n_edges:.0f} edges.")
     if torch.cuda.is_available() and other_params["device"] != "cpu":
         torch.cuda.empty_cache()
     idx_trn, idx_unlabeled, idx_val, idx_test = split(data_params, y)
@@ -230,6 +232,7 @@ def run(data_params: Dict[str, Any],
     A = torch.tensor(A, dtype=dtype, device=device)
     y = torch.tensor(y, device=device)
     n_classes = int(y.max() + 1)
+    data_dim = X.shape[1]
 
     idx_labeled = np.concatenate((idx_trn, idx_val)) 
     # idx of labeled nodes in nodes known during training (for semi-supervised)
@@ -260,11 +263,21 @@ def run(data_params: Dict[str, Any],
     
     # Poisoning Certificate
     svm_alpha = ntk.svm
-    is_robust_l, obj, opt_status, y_opt_l, y_test_worst_obj = utils.certify_collective_robust_label(
-            idx_labeled, idx_test, ntk_test, y, y_pred,
-            svm_alpha, certificate_params, l_flip=delta, C=model_params["regularizer"], M=1e3, Mprime=1e3
+    if "save_dir" in certificate_params:
+        save_params = dict(label=model_params["label"], seed=seed,
+                           dataset=data_params["dataset"],
+                           save_dir=certificate_params["save_dir"])
+    else:
+        save_params = None
+    del A
+    del X
+    is_robust_l, obj, obj_bound, opt_status, y_opt_l, y_test_worst_obj = \
+    utils.certify_collective_robust_label(
+            idx_labeled, idx_test, ntk_test, y, y_pred, svm_alpha, certificate_params, 
+            l_flip=delta, C=model_params["regularizer"], M=1e3, Mprime=1e3,
+            save_params=save_params
     )
-    acc_cert = sum(is_robust_l) / y_pred.shape[0]
+    acc_cert = (y_pred.shape[0] - obj_bound) / y_pred.shape[0]
     acc_cert_u = 0 #not implemented
     logging.info(f"Certified accuracy (poisoning): {acc_cert}")
 
@@ -298,6 +311,8 @@ def run(data_params: Dict[str, Any],
             y_true_cls = (y[idx_test] * 2 - 1).numpy(force=True).tolist(),
             y_pred_logit = y_pred.numpy(force=True).tolist(),
             y_worst_obj = y_test_worst_obj.tolist(),
+            obj = obj,
+            obj_bound = obj_bound,
             y_is_robust = is_robust_l,
             y_opt_status = opt_status,
             y_flip = y_opt_l, 
@@ -310,7 +325,7 @@ def run(data_params: Dict[str, Any],
             csbm_mu = mu[0].item(),
             csbm_p = p,
             csbm_q = q,
-            data_dim = X.shape[1],
+            data_dim = data_dim,
             # other statistics ntk / pred
             min_ypred = min_ypred,
             max_ypred = max_ypred,
@@ -334,6 +349,8 @@ def run(data_params: Dict[str, Any],
             y_true_cls = (y[idx_test] * 2 - 1).numpy(force=True).tolist(),
             y_pred_logit = y_pred.numpy(force=True).tolist(),
             y_worst_obj = y_test_worst_obj.tolist(),
+            obj = obj,
+            obj_bound = obj_bound,
             y_is_robust = is_robust_l,
             y_opt_status = opt_status,
             y_flip = y_opt_l, 
@@ -343,7 +360,7 @@ def run(data_params: Dict[str, Any],
             idx_labeled = idx_labeled.tolist(),
             idx_test = idx_test.tolist(),
             # data statistics
-            data_dim = X.shape[1],
+            data_dim = data_dim,
             # other statistics ntk / pred
             min_ypred = min_ypred,
             max_ypred = max_ypred,

exp_ntk_certify_label_multiclass_onenode.py
@@ -80,11 +80,12 @@ def config():
     )
 
     certificate_params = dict(
+        target_idx = 0, #0-based!
+        # n_targets_per_class = 10,
         n_adversarial = 10, # number adversarial nodes
         method = "XXT",
         perturbation_model = "linf",
-        delta = 0.01, # l0: local budget = delta * feature_dim
-        delta_absolute = True, # if false interpreted as % of 2*mu
+        delta = 0.01, 
         attack_nodes = "test", # "train", "all"
     )
 
@@ -221,14 +222,21 @@ def run(data_params: Dict[str, Any],
     if torch.cuda.is_available() and other_params["device"] != "cpu":
         torch.cuda.empty_cache()
     idx_trn, idx_unlabeled, idx_val, idx_test = split(data_params, y)
-    if len(idx_unlabeled) != 0:
-        idx_test = np.concatenate((idx_unlabeled, idx_test))
     X = torch.tensor(X, dtype=dtype, device=device)
     A = torch.tensor(A, dtype=dtype, device=device)
     y = torch.tensor(y, device=device)
     n_classes = int(y.max() + 1)
 
     idx_labeled = np.concatenate((idx_trn, idx_val)) 
+    idx_unlabeled = np.concatenate((idx_unlabeled, idx_test))
+    # Pick target node
+    target_idx = certificate_params["target_idx"] 
+    target_class = target_idx % n_classes
+    y_mask_target_cls = y[idx_unlabeled] == target_class
+    idx_targets = rng.permutation(idx_unlabeled[y_mask_target_cls])
+    step = int(target_idx / n_classes)
+    idx_test = idx_targets[step] 
+    idx_test = np.array([idx_test])
     # idx of labeled nodes in nodes known during training (for semi-supervised)
     if not data_params["learning_setting"] == "transductive":
         assert False, "Only transductive setting supported"
@@ -258,9 +266,9 @@ def run(data_params: Dict[str, Any],
     
     # Poisoning Certificate
     svm_alpha = ntk.svm
-    is_robust_l, obj_l, opt_status_l, y_opt_l = utils.certify_robust_label(
+    is_robust_l, y_opt_l = utils.certify_robust_label_one_vs_all(
             idx_labeled, idx_test, ntk_test, y, y_pred,
-            svm_alpha, certificate_params, l_flip=delta,
+            svm_alpha, certificate_params, l_flip=delta, 
             C=model_params["regularizer"], M=1e3, Mprime=1e3
     )
     acc_cert = sum(is_robust_l) / y_pred.shape[0]
@@ -285,11 +293,6 @@ def run(data_params: Dict[str, Any],
 
     if torch.cuda.is_available() and other_params["device"] != "cpu":
         torch.cuda.empty_cache()
-        
-    if mu is None:
-        mu = np.array([0])
-        p = 0
-        q = 0
     return dict(
         # general statistics
         accuracy_test = acc,
@@ -299,21 +302,19 @@ def run(data_params: Dict[str, Any],
         delta = delta,
         delta_absolute = delta, #legacy
         # node-wise pois. robustness statistics
-        y_true_cls = (y[idx_test] * 2 - 1).numpy(force=True).tolist(),
-        y_pred_logit = y_pred.numpy(force=True).tolist(),
-        y_worst_obj = obj_l,
-        y_is_robust = is_robust_l,
-        y_opt_status = opt_status_l,
-        y_flip = y_opt_l, 
+        y_true_cls = (2*y[idx_test]-1).numpy(force=True).tolist(),
+        y_pred_logit = y_pred[0].numpy(force=True).tolist(),
+        y_is_robust = is_robust_l[0],
+        y_flip = y_opt_l[0], 
         # split statistics
         idx_train = idx_trn.tolist(),
         idx_val = idx_val.tolist(),
         idx_labeled = idx_labeled.tolist(),
         idx_test = idx_test.tolist(),
         # data statistics
-        csbm_mu = mu[0].item(),
-        csbm_p = p,
-        csbm_q = q,
+        csbm_mu = 0,
+        csbm_p = 0,
+        csbm_q = 0,
         data_dim = X.shape[1],
         # other statistics ntk / pred
         min_ypred = min_ypred,

src/utils.py
@@ -1,5 +1,6 @@
 import logging
 from typing import Tuple, Union, Sequence
+import os
 
 from jaxtyping import Float, Integer
 import numpy as np
@@ -1198,18 +1199,16 @@ def certify_robust_label_one_vs_all(idx_labeled, idx_test, ntk, y,
         alpha[alpha_mask] = 0
         alpha_nz_mask = alpha>0 
         eq_constraint = y_labeled_*((ntk_labeled * alpha)@y_labeled_) - 1
-        eq_constraint[np.abs(eq_constraint) < MILP_INT_FEAS_TOL] = 0 # Added bec. nec. for real data, remove if results in problems
+        eq_constraint[np.abs(eq_constraint) < MILP_FEASIBILITY_TOL] = 0 # Added bec. nec. for real data, remove if results in problems
         u_start = np.zeros(alpha.shape[0], dtype=np.float64)
         v_start = np.zeros(alpha.shape[0], dtype=np.float64)
         u_start[~alpha_nz_mask] = eq_constraint[~alpha_nz_mask]
         v_start[alpha_nz_mask] = -eq_constraint[alpha_nz_mask]
-        u_start[u_start<globals.zero_tol] = 0
-        v_start[v_start<globals.zero_tol] = 0
         s_start = np.zeros(alpha.shape[0], dtype=np.int64)
-        u_nz_mask = u_start > 0
+        u_nz_mask = u_start > globals.zero_tol 
         s_start[u_nz_mask] = 1
         t_start = np.zeros(alpha.shape[0], dtype=np.int64)
-        v_nz_mask = v_start > 0
+        v_nz_mask = v_start > globals.zero_tol 
         t_start[v_nz_mask] = 1
         u_start_d[k] = u_start
         v_start_d[k] = v_start
@@ -1217,14 +1216,13 @@ def certify_robust_label_one_vs_all(idx_labeled, idx_test, ntk, y,
         t_start_d[k] = t_start
         alpha_l.append(alpha)
         assert (alpha<0).sum() == 0
-        assert (u_start<0).sum() == 0
-        assert (v_start<0).sum() == 0
-        assert ((y_labeled_*((ntk_labeled * alpha)@y_labeled_) - 1 - u_start + v_start) > globals.zero_tol).sum() == 0
-        assert (u_start-M*s_start > globals.zero_tol).sum() == 0
-        assert (alpha-C*(1-s_start) > globals.zero_tol).sum() == 0
-        assert (v_start-Mprime*t_start > globals.zero_tol).sum() == 0
-        assert (-alpha-1+t_start > globals.zero_tol).sum() == 0
-        #assert (-alpha+C*t_start > globals.zero_tol).sum() == 0
+        assert (u_start<-globals.zero_tol).sum() == 0 
+        assert (v_start<-globals.zero_tol).sum() == 0 
+        assert ((y_labeled_*((ntk_labeled * alpha)@y_labeled_) - 1 - u_start + v_start) > MILP_FEASIBILITY_TOL).sum() == 0
+        assert (u_start-M*s_start > MILP_FEASIBILITY_TOL).sum() == 0
+        assert (alpha-C*(1-s_start) > MILP_FEASIBILITY_TOL).sum() == 0
+        assert (v_start-Mprime*t_start > MILP_FEASIBILITY_TOL).sum() == 0
+        assert (-alpha-1+t_start > MILP_FEASIBILITY_TOL).sum() == 0
 
     obj_l = []
     is_robust_l = []
@@ -1428,8 +1426,8 @@ def certify_robust_label_one_vs_all(idx_labeled, idx_test, ntk, y,
                     else:
                         if m.ObjBound >= stop_obj:
                             is_robust = False
-                            m.dispose()
                             y_opt_l.append(y_b.X.tolist())
+                            m.dispose()
                             break
                 #obj_l.append(m.ObjVal)
                 #opt_status_l.append(m.Status)
@@ -1452,7 +1450,7 @@ def certify_robust_label_one_vs_all(idx_labeled, idx_test, ntk, y,
 
 def certify_collective_robust_label(idx_labeled, idx_test, ntk, y, 
                          y_pred, svm_alpha, certificate_params, l_flip=0.2, C=1, M=1e4, Mprime=1e4, 
-                         milp=True):
+                         milp=True, model_params=None, save_params=None):
     """TODO: Create documentation 
     """
     if isinstance(svm_alpha, torch.Tensor):
@@ -1617,12 +1615,14 @@ def certify_collective_robust_label(idx_labeled, idx_test, ntk, y,
         m.Params.FeasibilityTol = MILP_FEASIBILITY_TOL
         m.Params.OptimalityTol = MILP_OPTIMALITY_TOL
         m.Params.NumericFocus = 0
+        if "SoftMemLimit" in certificate_params:
+            m.Params.SoftMemLimit = certificate_params["SoftMemLimit"]
         if "NodeLimit" in certificate_params:
             m.Params.NodeLimit = certificate_params["NodeLimit"] # Explored node limit to stop 
         if "TimeLimit" in certificate_params:
             m.Params.TimeLimit = certificate_params["TimeLimit"]
         # m.Params.MIPGap = 1e-4
-        # m.Params.MIPGapAbs = 1e-4
+        m.Params.MIPGapAbs = 0.99
         # m.Params.Presolve = 0
         # m.Params.Aggregate = 0 #aggregation level in presolve
         if "MIPFocus" in certificate_params:
@@ -1682,9 +1682,18 @@ def certify_collective_robust_label(idx_labeled, idx_test, ntk, y,
         
         is_robust_l = (1-count.X).tolist()
         obj = m.ObjVal
+        obj_bound = m.ObjBound
         opt_status = m.Status
         y_test_worst_obj = ntk_unlabeled @ z.X
         y_opt_l = y_b.X.tolist()
+        if save_params is not None:
+            if opt_status == 9 or opt_status == 17:
+                base_filename = save_params["dataset"] + "_" \
+                                + save_params["label"] + "_" \
+                                + str(save_params["seed"])
+                solution_file = os.path.join(save_params["save_dir"], base_filename + ".sol")
+                logging.info(f"Opt Status {opt_status}, saving solution to {solution_file}.")
+                m.write(solution_file)
         # print('y initial ', y_labeled_)
         # print('y opt ', y.X)
         # print('actual flips ', -((y.X*y_labeled_)-1)@v_ones)
@@ -1696,4 +1705,4 @@ def certify_collective_robust_label(idx_labeled, idx_test, ntk, y,
     except AttributeError as a:
         logging.error(f"Encountered an attribute error {a.errno}: {a}")
         return
-    return is_robust_l, obj, opt_status, y_opt_l, y_test_worst_obj
+    return is_robust_l, obj, obj_bound, opt_status, y_opt_l, y_test_worst_obj